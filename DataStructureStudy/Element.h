ed032810023e667f51e0b088195fba68752d6100
blaxsior <heejun2798@gmail.com>
2021-04-14 19:52:38 +0900

    Tree
    left-child-right-sibiling
    Binary Tree
    full Binary Tree
    complete Binary Tree
    
    Tree에 대한 개념들 정리

--- DataStructureStudy/Element.h
+++ DataStructureStudy/Element.h
@@ -2,7 +2,7 @@
 //	int value;
 //}element;
 
-typedef int element;
+typedef tree_pointer element;
 
 typedef struct _node0
 {
@@ -14,18 +14,29 @@ typedef struct _node0
 typedef struct _node1
 {
 	element item;
-	struct _node1* before;
-	struct _node1* next;
+	struct _node1* Llink;
+	struct _node1* Rlink;
 }Node_R;
 //Node round trip
 
 typedef Node_O Node;
 
-//노드의 기본 값을 지정하고 새로 만들어진 노드를 반환한다.
+//단방향 노드의 기본 값을 지정하고 새로 만들어진 노드를 반환한다.
 Node_O* NodeInit(element item)
 {
 	Node_O* node = (Node_O*)malloc(sizeof(Node_O));
 	node->item = item;
 	node->next = NULL;
+	return node;
+}
+
+//양방향 노드의 기본 값을 지정하고 새로 만들어진 노드를 반환한다.
+Node_R* RNodeInit(element item)
+{
+	Node_R* node = (Node_R*)malloc(sizeof(Node_R));
+	node->item = item;
+	node->Llink = NULL;
+	node->Rlink = NULL;
+
 	return node;
 }
--- DataStructureStudy/main.c
+++ DataStructureStudy/main.c
@@ -1,23 +1,8 @@
-#include "LinkedStack.h"
-
+#include "Tree.h"
+#include <stdio.h>
 int main()
 {
-	LinkedStack stack;
-	LStackInit(&stack);
-	LSPop(&stack);
-	LSPush(&stack, 10);
-	LSPush(&stack, 11);
-	LSPush(&stack, 12);
-	LSPush(&stack, 13);
-	LSPush(&stack, 14);
-	LSPush(&stack, 15);
-	LSPush(&stack, 16);
-
-
-	while (stack.top != NULL)
-	{
-		printf("%d\n", LSPop(&stack));
-	}
-
-	return 0;
+	printf("%d\n", getParent(3));
+	printf("%d\n", getLeftChild(3));
+	printf("%d\n", getRightChild(3));
 }
