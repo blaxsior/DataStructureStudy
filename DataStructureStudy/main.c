37af1e8632e59935c15bc801bdc8fd2ba3ce6123
blaxsior <heejun2798@gmail.com>
2021-04-27 21:47:51 +0900

    Binary Search Tree 예제 삭제 기능이 실제로 작동하는지, level order Traverse로 보여주고 있다.

--- DataStructureStudy/main.c
+++ DataStructureStudy/main.c
@@ -1,8 +1,22 @@
-#include "Tree.h"
+#include "BSTree.h"
 #include <stdio.h>
+
+
 int main()
 {
-	printf("%d\n", getParent(3));
-	printf("%d\n", getLeftChild(3));
-	printf("%d\n", getRightChild(3));
+	BST_pointer ptr = RNodeInit(40);
+	insert_node(ptr, 20);
+	insert_node(ptr, 60);
+	insert_node(ptr, 10);
+	insert_node(ptr, 30);
+	insert_node(ptr, 50);
+	insert_node(ptr, 70);
+	insert_node(ptr, 45);
+	insert_node(ptr, 55);
+	insert_node(ptr, 52);
+
+	level_orderTraverseBST(ptr);
+	putchar('\n');
+	delete_node(ptr, 60);
+	level_orderTraverseBST(ptr);
 }
